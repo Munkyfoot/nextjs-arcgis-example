bafb96e1a4db1ae2231737fe69ca52fff160c52b
Munkyfoot <wopas182@gmail.com>
2024-03-07 14:00:44 -0800

    Make the padding on the main page responsive to improve look across devices.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import DynamicMap from "@/components/dynamic-map"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center p-6 md:p-12 lg:p-24">
       <DynamicMap />
     </main>
   )
